7cf21c5642665bd1280fcab904d333f4f78a0189
rifatul97 <rifatulkarim1997@gmail.com>
2021-12-17 19:28:05 -0500

    added topnavbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <p>hello world.</p>    
+    <nav>
+      <h1>RIFATUL KARIM</h1>
+      <ul class="navigation">
+        <li><a href='=#about' class="nav-link">About</li>
+        <li><a href='=#skills' class="nav-link">Skills</li>
+        <li><a href='=#projects' class="nav-link">Projects</li>
+        <li><a href='=#contact' class="nav-link">Contact</li>
+      </ul>
+      
+    </nav>  
   );
 }
 
